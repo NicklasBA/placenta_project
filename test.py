204e503854cd98542443067f5e1db11912fc8e9d
PeterKampen <s183993@student.dtu.dk>
2021-11-24 15:40:11 +0100

    turn off slow

--- test.py
+++ test.py
@@ -234,7 +234,8 @@ def find_files(data_dir, folder):
     for ends, seq in list(zip(all_end, all_seq)):
         if len(seq) < 5*15:
             if ends[0] > 25 and ends[-1] < 43300-25:
-                all_new.append([all_old[idx] for idx in range(ends[0]-25, ends[-1]+25)])
+                all_new.append([all_old[idx] for idx in range(ends[0]-25, ends[0])] +
+                               seq + [all_old[idx] for idx in range([ends[-1]+1,ends[-1]+25])])
         else:
             all_new.append(seq)
     return all_new
