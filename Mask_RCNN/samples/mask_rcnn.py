8e19f6bf5e5329b6b6557b4c94951bc0197a718c
GustavAls <gustavals1998@gmail.com>
2021-11-29 14:11:32 +0100

    mask_rcnn done

--- Mask_RCNN/samples/mask_rcnn.py
+++ Mask_RCNN/samples/mask_rcnn.py
@@ -1,3 +1,26 @@
+"""
+Mask R-CNN
+
+Copyright (c) 2018 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+
+------------------------------------------------------------
+
+Usage: import the module (see Jupyter notebooks for examples), or run from
+       the command line as such:
+
+    # Train a new model starting from pre-trained COCO weights
+    python3 mask_rcnn.py train --dataset=/path/to/dataset --weights=coco
+
+    # Resume training a model that you had trained earlier
+    python3 mask_rcnn.py train --dataset=/path/to/dataset --weights=last
+
+    # Train a new model starting from ImageNet weights
+    python3 mask_rcnn.py train --dataset=/path/to/dataset --weights=imagenet
+
+"""
+
 import os
 import sys
 import random
@@ -5,6 +28,9 @@ import math
 import re
 import time
 import numpy as np
+import skimage
+import datetime
+import json
 import cv2
 import matplotlib
 import matplotlib.pyplot as plt
@@ -15,64 +41,56 @@ from Mask_RCNN.mrcnn import visualize
 from Mask_RCNN.mrcnn.model import log
 import pickle
 
-"""
-Er begyndt ud fra train_shapes.py, men opdagede balloon.py allerede har gjort det 
-meste af arbejdet for os, så man kunne evt starte fra den i stedet.
-Funktionerne load_image, load_mask, image reference og load_balloon (måske load_rbc) skal bare ændres, og 
-så skal der sættes et par hyperparametre
-"""
+# Root directory of the project
+ROOT_DIR = r'/home/s183993/placenta_project/Mask_RCNN/'
 
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+############################################################
+#  Configurations
+############################################################
 
 
 class PlacentaConfig(Config):
-    """Configuration for training on the toy shapes dataset.
-    Derives from the base Config class and overrides values specific
-    to the toy shapes dataset.
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
     """
     # Give the configuration a recognizable name
     NAME = "placenta"
 
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 8
-
-    # Number of classes (including background)
-    NUM_CLASSES = 1 + 2  # background + 3 shapes
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 4
 
     # Use small images for faster training. Set the limits of the small side
     # the large side, and that determines the image shape.
     IMAGE_MIN_DIM = 150
     IMAGE_MAX_DIM = 800
 
-    # Use smaller anchors because our image and objects are small
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128, 256, 512)  # anchor side in pixels
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 2  # Background + D + NS
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
 
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
     TRAIN_ROIS_PER_IMAGE = 32
 
-    # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 100
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
 
-    # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 5
-
-
-class InferenceConfig(PlacentaConfig):
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-
-def get_ax(rows=1, cols=1, size=8):
-    """Return a Matplotlib Axes array to be used in
-    all visualizations in the notebook. Provide a
-    central point to control graph sizes.
-
-    Change the default size attribute to control the size
-    of rendered images
-    """
-    _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
-    return ax
+############################################################
+#  Dataset
+############################################################
 
 class PlacentaDataset(utils.Dataset):
     """
@@ -80,7 +98,7 @@ class PlacentaDataset(utils.Dataset):
     the format present for the placenta dataset
     """
     def load_rbc(self, dataset_dir, subset):
-        """Load a subset of the Balloon dataset.
+        """Load a subset of dataset.
                 dataset_dir: Root directory of the dataset.
                 subset: Subset to load: train, val or train
                 """
@@ -143,139 +161,112 @@ class PlacentaDataset(utils.Dataset):
             mask[:, :, i] += self.fill_bbox(bbox, shape = (info['height'], info['width']))
 
         class_ids = np.array([1 for _ in range(info['count'])])
-        """
-        Quite certain that we dont need the stuff below, ours is far more simple
-        """
-        # shapes = info['shapes']
-        # count = len(shapes)
-        # mask = np.zeros([info['height'], info['width'], count], dtype=np.uint8)
-        # for i, (shape, _, dims) in enumerate(info['shapes']):
-        #     mask[:, :, i:i+1] = self.draw_shape(mask[:, :, i:i+1].copy(),
-        #                                         shape, dims, 1)
-        # # Handle occlusions
-        # occlusion = np.logical_not(mask[:, :, -1]).astype(np.uint8)
-        # for i in range(count-2, -1, -1):
-        #     mask[:, :, i] = mask[:, :, i] * occlusion
-        #     occlusion = np.logical_and(occlusion, np.logical_not(mask[:, :, i]))
-        # # Map class names to class IDs.
-        # class_ids = np.array([self.class_names.index(s[0]) for s in shapes])
         return mask.astype(np.bool), class_ids.astype(np.int32)
 
 
-
-
-def detection(MODEL_DIR, dataset_train, dataset_val):
-    inference_config = InferenceConfig()
-    # Recreate the model in inference mode
-    model = modellib.MaskRCNN(mode="inference",
-                              config=inference_config,
-                              model_dir=MODEL_DIR)
-
-    # Get path to saved weights
-    # Either set a specific path or find last trained weights
-    # model_path = os.path.join(ROOT_DIR, ".h5 file name here")
-    model_path = model.find_last()
-
-    # Load trained weights
-    print("Loading weights from ", model_path)
-    model.load_weights(model_path, by_name=True)
-
-    # Test on a random image
-    image_id = random.choice(dataset_val.image_ids)
-    original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
-        modellib.load_image_gt(dataset_val, inference_config,
-                               image_id, use_mini_mask=False)
-
-    log("original_image", original_image)
-    log("image_meta", image_meta)
-    log("gt_class_id", gt_class_id)
-    log("gt_bbox", gt_bbox)
-    log("gt_mask", gt_mask)
-
-    visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id,
-                                dataset_train.class_names, figsize=(8, 8))
-
-    results = model.detect([original_image], verbose=1)
-
-    r = results[0]
-    visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'],
-                                dataset_val.class_names, r['scores'], ax=get_ax())
-
-
-def main():
-
-    # Root directory of the project
-    ROOT_DIR = r'/home/s183993/placenta_project/Mask_RCNN/'
-
-    # Import Mask RCNN
-    sys.path.append(ROOT_DIR)  # To find local version of the library
-
-    # Directory to save logs and trained model
-    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
-
-    # Local path to trained weights file
-    COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-    # Download COCO trained weights from Releases if needed
-    if not os.path.exists(COCO_MODEL_PATH):
-        utils.download_trained_weights(COCO_MODEL_PATH)
-    config = PlacentaConfig()
-    config.display()
-
-    # Training dataset
+def train(model):
+    """Train the model."""
+    # Training dataset.
     dataset_train = PlacentaDataset()
-    dataset_train.load_shapes(500, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+    dataset_train.load_rbc(args.dataset, "train")
     dataset_train.prepare()
 
     # Validation dataset
     dataset_val = PlacentaDataset()
-    dataset_val.load_shapes(50, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+    dataset_val.load_rbc(args.dataset, "val")
     dataset_val.prepare()
 
-    # Load and display random samples
-    image_ids = np.random.choice(dataset_train.image_ids, 4)
-    for image_id in image_ids:
-        image = dataset_train.load_image(image_id)
-        mask, class_ids = dataset_train.load_mask(image_id)
-        visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
-
-    model = modellib.MaskRCNN(mode="training", config=config,
-                              model_dir=MODEL_DIR)
-
-    # Which weights to start with?
-    init_with = "coco"  # imagenet, coco, or last
-
-    if init_with == "imagenet":
-        model.load_weights(model.get_imagenet_weights(), by_name=True)
-    elif init_with == "coco":
-        # Load weights trained on MS COCO, but skip layers that
-        # are different due to the different number of classes
-        # See README for instructions to download the COCO weights
-        model.load_weights(COCO_MODEL_PATH, by_name=True,
-                           exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
-                                    "mrcnn_bbox", "mrcnn_mask"])
-    elif init_with == "last":
-        # Load the last model you trained and continue training
-        model.load_weights(model.find_last(), by_name=True)
-
-    # Train the head branches
-    # Passing layers="heads" freezes all layers except the head
-    # layers. You can also pass a regular expression to select
-    # which layers to train by name pattern.
+    # *** This training schedule is an example. Update to your needs ***
+    # Since we're using a very small dataset, and starting from
+    # COCO trained weights, we don't need to train too long. Also,
+    # no need to train all layers, just the heads should do it.
+    print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=1,
+                epochs=30,
                 layers='heads')
 
-    # Fine tune all layers
-    # Passing layers="all" trains all layers. You can also
-    # pass a regular expression to select which layers to
-    # train by name pattern.
-    model.train(dataset_train, dataset_val,
-                learning_rate=config.LEARNING_RATE / 10,
-                epochs=2,
-                layers="all")
+############################################################
+#  Training
+############################################################
 
-    #detection(MODEL_DIR, dataset_train, dataset_val)
+if __name__ == '__main__':
+    import argparse
 
-if __name__ == "main":
-    main()
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN to detect balloons.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'splash'")
+    parser.add_argument('--dataset', required=False,
+                        metavar="/path/to/dataset/",
+                        help='Directory of the dataset')
+    parser.add_argument('--weights', required=True,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    args = parser.parse_args()
+
+    # Validate arguments
+    if args.command == "train":
+        assert args.dataset, "Argument --dataset is required for training"
+
+    print("Weights: ", args.weights)
+    print("Dataset: ", args.dataset)
+    print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = PlacentaConfig()
+    else:
+        class InferenceConfig(PlacentaConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+    # Train or evaluate
+    if args.command == "train":
+        train(model)
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train' or 'splash'".format(args.command))
