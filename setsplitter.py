44cf050701bcb4069314600efc1b3d818cb16d5d
NicklasBA <45656670+NicklasBA@users.noreply.github.com>
2021-12-20 13:47:05 +0100

    Update setsplitter.py

--- setsplitter.py
+++ setsplitter.py
@@ -14,17 +14,17 @@ FILES='/scratch/s183993/placenta/raw_data/videos_blackened_org_bbox_full/*_6mbar
 #FILES='/scratch/s183993/placenta/raw_data/Placenta_package/*_6mbar_500fps_*_*'
 # OUTDIR = "/home/s183993/placenta_project/outputs"
 paths = glob.glob(FILES)
-breakpoint()
+#breakpoint()
 df = pd.DataFrame({"paths":paths})
 df['DonorType'] = df.paths.str.slice(-13,-12)
 df['DonorCode'] = df.paths.str.slice(-14,-9)
 df['Num'] = df.paths.str.slice(-8,-4)
 df2 = df[df.DonorType == "D"]
-df3 = df[df.DonorType == "S"]
+df3 = df[df.DonorType == "N"]
 num_d = sum(df.DonorType == 'D')
-num_NS = sum(df.DonorType == 'S')
+num_NS = sum(df.DonorType == 'N')
 df2 = df2.groupby(df.DonorCode).count()
 df3 = df3.groupby(df.DonorCode).count()
-breakpoint()
+#breakpoint()
 df2.to_csv("D.csv")
 df3.to_csv("NS.csv")
