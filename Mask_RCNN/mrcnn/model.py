c5e53367ad0398f8d84de508a19503d3ce33edf3
PeterKampen <s183993@student.dtu.dk>
2021-12-20 13:26:25 +0100

    quick file move

--- Mask_RCNN/mrcnn/model.py
+++ Mask_RCNN/mrcnn/model.py
@@ -2527,6 +2527,7 @@ class MaskRCNN(object):
             self.keras_model.predict([molded_images, image_metas, anchors], verbose=0)
         # Process detections
         results = []
+        breakpoint()
         for i, image in enumerate(images):
             final_rois, final_class_ids, final_scores, final_masks =\
                 self.unmold_detections(detections[i], mrcnn_mask[i],
