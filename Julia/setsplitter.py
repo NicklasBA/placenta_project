8434ed5c7c00190e1adc26164c71d3635e85dae3
JSecher <Joachim@Secher-fam.dk>
2022-02-22 10:47:46 +0100

    Updated setsplitter

--- Julia/setsplitter.py
+++ Julia/setsplitter.py
@@ -7,24 +7,70 @@ Created on Mon Nov 29 20:22:32 2021
 
 import glob
 import pandas as pd
+import re
+import os
+import argparse
 
+print( "Creating CSV from folders")
+DEFAULT_FILES = '/scratch/s183993/placenta/raw_data/videos_blackened_org_bbox_full/*'
+# DEFAULT_FILES =  '/Users/joachimsecher/Downloads/placenta_images/*'
 
-print( "Creating videos from images")
-#FILES='/scratch/s183993/placenta/raw_data/videos_blackened_org_bbox_full/*_6mbar_500fps_*_*'
-FILES='/scratch/s183993/placenta/raw_data/Placenta_package/*_6mbar_500fps_*_*'
-# OUTDIR = "/home/s183993/placenta_project/outputs"
+parser = argparse.ArgumentParser(description="Create CSVs for JULIA Training split creator")
+parser.add_argument("-i", '--input',
+                    default='/Users/joachimsecher/Downloads/placenta_images/*',
+                    help=f"Directory with found single cell videos. Default: {DEFAULT_FILES}")
+parser.add_argument("-o", '--output',
+                    default='',
+                    help=f"Output directory. Default is CWD")
+
+args = parser.parse_args()
+FILES = args.input
+output_dir = args.output
+
 paths = glob.glob(FILES)
-#breakpoint()
 df = pd.DataFrame({"paths":paths})
-df['DonorType'] = df.paths.str.slice(-13,-12)
-df['DonorCode'] = df.paths.str.slice(-14,-9)
-df['Num'] = df.paths.str.slice(-8,-4)
-df2 = df[df.DonorType == "D"]
-df3 = df[df.DonorType == "N"]
-num_d = sum(df.DonorType == 'D')
-num_NS = sum(df.DonorType == 'N')
-df2 = df2.groupby(df.DonorCode).count()
-df3 = df3.groupby(df.DonorCode).count()
+
+def donor_code(path):
+    code = re.search("(D)[0-9]{1,4}|(NS)[0-9]{1,4}", path)
+    try:
+        code = code[0]
+    except TypeError:
+        code = None
+    return code
+
+def donor_type(path):
+    dtype = donor_code(path)
+    if dtype is None:
+        pass
+    else:
+        if "D" in dtype.upper():
+            dtype = "D"
+        elif "NS" in dtype.upper():
+            dtype = "NS"
+
+    return dtype
+
+def donor_id(path):
+    code = re.search("[0-9]{1,4}", donor_code(path))
+    try:
+        code = int(code[0])
+    except TypeError:
+        code = None
+    return code
+
+
+df['DonorCode'] = df.paths.apply(donor_code)
+df['DonorType'] = df.paths.apply(donor_type)
+# df[] = df.paths.apply(donor_id)
+df_D = df[df.DonorType == "D"]
+df_NS = df[df.DonorType == "NS"]
+num_d = len(df_D)
+num_NS = len(df_NS)
+df_D["Num"] = df_D['Num'] = df.groupby('DonorCode')['DonorCode'].transform('count')
+df_NS["Num"] = df_NS.groupby(["DonorCode"])['DonorCode'].transform('nunique')
 #breakpoint()
-df2.to_csv("D.csv")
-df3.to_csv("NS.csv")
+df_D.to_csv(os.path.join(output_dir, "D.csv"))
+df_NS.to_csv(os.path.join(output_dir, "NS.csv"))
+
+print("Ignore the wranings above...")
+print("Saved data to 'D.csv' and 'NS.csv', now go run the julia script")
