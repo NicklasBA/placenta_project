6a13c440b87fe3a03d95eb0f1877d8a52cd52133
PeterKampen <s183993@student.dtu.dk>
2022-03-01 09:30:55 +0100

    config new

--- SlowFast/slowfast/datasets/kinetics.py
+++ SlowFast/slowfast/datasets/kinetics.py
@@ -117,6 +117,8 @@ class Kinetics(torch.utils.data.Dataset):
                     self._video_meta[clip_idx * self._num_clips + idx] = {}
 
                 if self.cfg.get('small', None) is not None:
+                    if clip_idx == 0:
+                        print("using only the first 100 files")
                     if clip_idx == 100:
                         break
 
